7ea61f7ac205b38d06b67b5e6c437745cfc11f72
Papgy100 <123894339+Papgy@users.noreply.github.com>
2025-04-11 22:30:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.0';
+import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.5.0';
 
 let generator;
 window.generatedFiles = {};
